b727bdbe1bd29dcf1a969933db9fe8a0e1b08b6a
Eric Castillo <ecastillo11@gmail.com>
2018-05-27 13:16:34 -0500

    Create test articles and fix cases that crash.

--- RSS App/Controller/ArticleViewController.swift
+++ RSS App/Controller/ArticleViewController.swift
@@ -26,26 +26,29 @@ class ArticleViewController: UIViewController, WKUIDelegate, WKNavigationDelegat
     @IBOutlet weak var bodyContent: ArticleAttributedTextContentView!
     
     var article: Article!
-    
-    var articleContent = """
-                        The Google News app is a reimagining and revamp to the existing Google Newsstand Play app that was previously available via the iOS App Store. It's been entirely overhauled though, with a simple, clean interface that's fairly similar to the look of Apple News with a dedicated navigation bar at the bottom.
-                        """
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         articleTitle.text = article.title
-        
+        featuredImage.sd_setImage(with: article.featuredImageURL, placeholderImage: nil)
         if let url = article.url {
             let domain = url.host?.replacingOccurrences(of: "^www\\.*", with: "", options: .regularExpression)
             website.setTitle(domain, for: .normal)
         }
-        
         date.text = article.date?.timeAgoSinceNow
         
-        featuredImage.sd_setImage(with: article.featuredImageURL, placeholderImage: nil)
-        
-        test()
+        if let html = article.content {
+            let data = html.data(using: .utf8)
+            let defaultStyleSheet = DTCSSStylesheet(styleBlock: "br{display: none}")
+            let defaultFontFamily = UIFont.systemFont(ofSize: 16).familyName
+            let attrString = NSAttributedString(htmlData: data, options:[DTUseiOS6Attributes: true, DTDefaultStyleSheet: defaultStyleSheet, DTDefaultFontFamily: defaultFontFamily, DTDefaultFontSize: 16, DTDefaultLineHeightMultiplier: 1.4], documentAttributes:nil)
+            let muteAttString = NSMutableAttributedString(attributedString: attrString!)
+            
+            bodyContent.shouldDrawImages = false
+            bodyContent.delegate = self
+            bodyContent.attributedString = muteAttString
+        }
     }
 
     
@@ -54,70 +57,14 @@ class ArticleViewController: UIViewController, WKUIDelegate, WKNavigationDelegat
         self.present(svc, animated: true, completion: nil)
     }
     
-    func test() {
-        if var html = article.content {
-
-            var string = """
-                        The Worldwide Developers Conference is just about a week and a half away, and while we've heard some rumors on what we might see in iOS 12 and macOS 10.14, watchOS 5, the next-generation software update for the Apple Watch, remains a total mystery.
-                        <br/>
-
-                        <br/>
-                        With no idea what to expect, we asked <em>MacRumors</em> readers what new features and tweaks they would most like to see in the watchOS 5 update.
-                        <br/>
-
-                        <br/>
-                        <img src="http://cdn.macrumors.com/article-new/2018/05/watchos5-800x395.jpg" alt="" width="800" height="395" class="aligncenter size-large wp-image-637612" />
-                        <br/>
-                        <ul>
-                        <br/>
-                             <li><strong>Live step count complication</strong> - <em>MacRumors</em> reader <a href="https://forums.macrumors.com/threads/what-do-you-want-in-watchos-5.2119197/#post-26066451">Breezygirl</a> would like to see Apple add a live step complication that lets you see how many steps you've completed at a glance, rather than just a complication that lets you know how close you've come to hitting your activity ring goal.</li><br>
-                        <br/>
-                             <li><strong>Third-party watch faces</strong> - Third-party watch faces are highly desired by most Apple Watch owners on the forums, but so far, Apple has kept the Apple Watch locked down to control the design and interface of the device. As <em>MacRumors</em> reader <a href="https://forums.macrumors.com/threads/what-do-you-want-in-watchos-5.2119197/#post-26066887">Relentless Power</a> suggests, a watch face store that includes a variety of watch faces from third-party developers and companies would be great.</li><br>
-                        <br/>
-                             <li><strong>Activity app improvements</strong> - Right now, the Activity app requires you to hit your goals each and every day to keep a streak going, which can be difficult at times and allows for no rest. <em>MacRumors</em> reader <a href="https://forums.macrumors.com/threads/what-do-you-want-in-watchos-5.2119197/#post-26066994">SoYoung</a> would like to be able to set rest days.</li><br>
-                        <br/>
-                             <li><strong>Workout app improvements</strong> - In the same vein, <em>MacRumors</em> reader <a href="https://forums.macrumors.com/threads/what-do-you-want-in-watchos-5.2119197/#post-26067372">Rbart</a> is hoping for a better workout app for running that's closer in design to Strava with additional statistics, a complete history, best performances, and more. <a href="https://forums.macrumors.com/threads/what-do-you-want-in-watchos-5.2119197/#post-26068192">Honglong1976</a>, meanwhile, would like to see automatic activity detection to alleviate the need to start a workout.</li><br>
-                        <br/>
-                             <li><strong>Podcasts for Apple Watch</strong> - Multiple <em>MacRumors</em> <a href="https://forums.macrumors.com/threads/what-do-you-want-in-watchos-5.2119197/#post-26067031">readers</a> would like to see a dedicated Podcasts app on the Apple Watch for listening to podcasts on the wrist-worn device.</li><br>
-                        <br/>
-                             <li><strong>Off-wrist Notifications indicator</strong> - <em>MacRumors</em> reader <a href="https://forums.macrumors.com/threads/what-do-you-want-in-watchos-5.2119197/#post-26066619">Lennyvalentin</a> would like to see the Apple Watch better able to keep track of incoming notifications even when off the wrist, with those notifications still showing up but with an indication to note that they were received while the Apple Watch was idle.</li><br>
-                        <br/>
-                             <li><strong>Proximity notifications</strong> - There's no way to set the Apple Watch to ping when it goes out of range of the iPhone, a feature <em>MacRumors</em> reader <a href="https://forums.macrumors.com/threads/what-do-you-want-in-watchos-5.2119197/#post-26068190">Justiny</a> would like to see as a way to keep track of the iPhone and get a reminder if it's left behind.</li><br>
-                        <br/>
-                             <li><strong>Sleep tracking</strong> - This one is probably a long shot given that Apple suggests people charge their Apple Watches at night, but <em>MacRumors</em> readers would <a href="https://forums.macrumors.com/threads/what-do-you-want-in-watchos-5.2119197/#post-26068568">like to see</a> native sleep tracking capabilities.</li><br>
-                        <br/>
-                             <li><strong>Always-on display</strong> - Given battery constraints, Apple has never implemented an always-on display for the Apple Watch, which is another <a href="https://forums.macrumors.com/threads/what-do-you-want-in-watchos-5.2119197/#post-26068960">highly desired feature</a>. The Apple Watch display comes on when the wrist is raised, but it would be nice to have always-on access to the time as is possible with a traditional watch.</li><br>
-                        <br/>
-                             <li><strong>Better health analysis and suggestions</strong> - <em>MacRumors</em> reader <a href="https://forums.macrumors.com/threads/what-do-you-want-in-watchos-5.2119197/#post-26069924">Bluecoast</a> would like to see Apple better take advantage of the health information it collects with the watch to add recommendations and coaching for those who are aiming to meet health goals, as well as deeper analysis.</li>
-                        <br/>
-                        </ul>
-                        <br/>
-                        Is there something you're hoping to see in watchOS 5 that didn't make it on our list? Make sure to let us know in the comments.<br/><br/><div class="linkback">Related Roundups: <a href="https://www.macrumors.com/roundup/apple-watch/">Apple Watch</a>, <a href="https://www.macrumors.com/roundup/watchos-4/">watchOS 4</a></div><div class="linkback">Buyer's Guide: <a href="https://buyersguide.macrumors.com/#Apple_Watch">Apple Watch (Neutral)</a></div><br/><a href="https://forums.macrumors.com/threads/watchos-5-macrumors-readers-wishlist.2120199/">Discuss this article</a> in our forums<br/><br/><div class="feedflare">
-                        <a href="http://feeds.macrumors.com/~ff/MacRumors-All?a=RX63eHcExOg:hdMVyQyuq84:6W8y8wAjSf4"><img src="http://feeds.feedburner.com/~ff/MacRumors-All?d=6W8y8wAjSf4" border="0"></img></a> <a href="http://feeds.macrumors.com/~ff/MacRumors-All?a=RX63eHcExOg:hdMVyQyuq84:qj6IDK7rITs"><img src="http://feeds.feedburner.com/~ff/MacRumors-All?d=qj6IDK7rITs" border="0"></img></a>
-                        </div><img src="http://feeds.feedburner.com/~r/MacRumors-All/~4/RX63eHcExOg" height="1" width="1" alt=""/>
-                        """
-            
-            let data = html.data(using: .utf8)
-            let defaultStyleSheet = DTCSSStylesheet(styleBlock: "br{display: none}")
-            let defaultFontFamily = UIFont.systemFont(ofSize: 16).familyName
-            let attrString = NSAttributedString(htmlData: data, options:[DTUseiOS6Attributes: true, DTDefaultStyleSheet: defaultStyleSheet, DTDefaultFontFamily: defaultFontFamily, DTDefaultFontSize: 16, DTDefaultLineHeightMultiplier: 1.4], documentAttributes:nil)
-            let muteAttString = NSMutableAttributedString(attributedString: attrString!)
-
-            bodyContent.shouldDrawImages = false
-            bodyContent.delegate = self
-            bodyContent.attributedString = muteAttString
-
-        }
-    }
-    
     
     //MARK: - Attachment views
     
     func attributedTextContentView(_ attributedTextContentView: DTAttributedTextContentView!, viewFor attachment: DTTextAttachment!, frame: CGRect) -> UIView! {
         if let imageAttachment = attachment as? DTImageTextAttachment {
-            print("found an image")
+            print("found an image of original size: \(imageAttachment.originalSize)")
             // if the attachment has a hyperlinkURL then this is currently ignored
-            if imageAttachment.originalSize != CGSize.zero {
+            if imageAttachment.originalSize.width > 0 && imageAttachment.originalSize.height > 0  {
                 let imageView = DTLazyImageView(frame: frame)
                 imageView.delegate = self
                 
@@ -130,12 +77,18 @@ class ArticleViewController: UIViewController, WKUIDelegate, WKNavigationDelegat
                 return imageView
             }
         } else if let iframeAttachment = attachment as? DTIframeTextAttachment {
-            print("found an iframe")
-            let aspectRatio = iframeAttachment.originalSize.width / iframeAttachment.originalSize.height
-            iframeAttachment.displaySize = CGSize(width: bodyContent.frame.width, height: bodyContent.frame.width / aspectRatio)
-            let videoView = DTWebVideoView(frame: frame)
-            videoView.attachment = iframeAttachment
-            return videoView;
+            print("found an iframe of original size: \(iframeAttachment.originalSize)")
+            if iframeAttachment.originalSize.width > 0 && iframeAttachment.originalSize.height > 0  {
+                // iframe has a height and width, so resize it to fit the screen
+                let aspectRatio = iframeAttachment.originalSize.width / iframeAttachment.originalSize.height
+                iframeAttachment.displaySize = CGSize(width: bodyContent.frame.width, height: bodyContent.frame.width / aspectRatio)
+                let videoView = DTWebVideoView(frame: frame)
+                videoView.attachment = iframeAttachment
+                return videoView
+            } else {
+                // iframe doesn't have both height and width, so set originalSize to zero to prevent it from displaying
+                iframeAttachment.originalSize = CGSize.zero
+            }
         }
         
         return nil
@@ -152,17 +105,9 @@ class ArticleViewController: UIViewController, WKUIDelegate, WKNavigationDelegat
         for case let oneAttachment as DTTextAttachment in bodyContent.layoutFrame.textAttachments(with: pred) {
             // update attachments that have no original size, that also sets the display size
             print(oneAttachment.contentURL)
-            if oneAttachment.originalSize == CGSize.zero {
-                print("equal to zero")
-                oneAttachment.originalSize = CGSize(width: bodyContent.frame.width, height: 10) //imageSize
-                didUpdate = true
-            } else {
-                print("not equal to zero")
-                //(oneAttachment as! DTTextAttachment).originalSize = size
-                let aspectRatio = size.width / size.height
-                oneAttachment.displaySize = CGSize(width: bodyContent.frame.width, height: bodyContent.frame.width / aspectRatio) //imageSize
-                didUpdate = true
-            }
+            let aspectRatio = size.width / size.height
+            oneAttachment.displaySize = CGSize(width: bodyContent.frame.width, height: bodyContent.frame.width / aspectRatio) //imageSize
+            didUpdate = true
         }
         if didUpdate {
             // layout might have changed due to image sizes
@@ -193,20 +138,11 @@ class ArticleViewController: UIViewController, WKUIDelegate, WKNavigationDelegat
     }
     
     
-    
-    
     override func viewWillLayoutSubviews() {
         print("view will layout subviews")
     
         bodyContent.layouter = nil
         bodyContent.relayoutText()
-    }
-    
-    override func viewDidLayoutSubviews() {
-        print("view did layout subviews")
-        
-        //bodyContent.layouter = nil
-        //bodyContent.relayoutText()
     }
 
 }
--- RSS App/Support Files/Dummy Data/dummyData.swift
+++ RSS App/Support Files/Dummy Data/dummyData.swift
@@ -10,13 +10,99 @@ import Foundation
 
 func dummyArticles() -> [Article] {
     var articles = [Article]()
-    
-    let articleWithNoImages = Article()
     let bundle = Bundle.main
+    var path: String?
     
-    let path = bundle.path(forResource: "noImagesInContent", ofType: "html")
-    try! articleWithNoImages.content = String(contentsOfFile: path!)
-    articles.append(articleWithNoImages)
+    let noFeaturedImage = Article()
+    noFeaturedImage.title = "No Featured Image"
+    noFeaturedImage.featuredImageURL = nil
+    articles.append(noFeaturedImage)
+    
+    let smallFeaturedImage = Article()
+    smallFeaturedImage.title = "Small Featured Image"
+    smallFeaturedImage.featuredImageURL = URL(string: "http://placehold.it/50x50")
+    articles.append(smallFeaturedImage)
+    
+    let longTitle = Article()
+    longTitle.title = "Long Title Integer posuere erat a ante venenatis dapibus posuere velit aliquet curabitur blandit tempus porttitor."
+    articles.append(longTitle)
+    
+    let longDomain = Article()
+    longDomain.title = "Long Domain"
+    longDomain.url = URL(string: "http://thisisaverlylongdomainnamethatprobablywontfit.com")
+    articles.append(longDomain)
+    
+    let smallImageInContent = Article()
+    smallImageInContent.title = "Small Image in Content"
+    path = bundle.path(forResource: "smallImageInContent", ofType: "html")
+    try! smallImageInContent.content = String(contentsOfFile: path!)
+    articles.append(smallImageInContent)
+    
+    let manyImages = Article()
+    manyImages.title = "Many Images"
+    path = bundle.path(forResource: "manyImages", ofType: "html")
+    try! manyImages.content = String(contentsOfFile: path!)
+    articles.append(manyImages)
+    
+    let imageNoWidth = Article()
+    imageNoWidth.title = "Image No Width"
+    path = bundle.path(forResource: "imageNoWidth", ofType: "html")
+    try! imageNoWidth.content = String(contentsOfFile: path!)
+    articles.append(imageNoWidth)
+    
+    let imageNoHeight = Article()
+    imageNoHeight.title = "Image No Height"
+    path = bundle.path(forResource: "imageNoHeight", ofType: "html")
+    try! imageNoHeight.content = String(contentsOfFile: path!)
+    articles.append(imageNoHeight)
+    
+    let imageNoWidthHeight = Article()
+    imageNoWidthHeight.title = "Image No Width or Height"
+    path = bundle.path(forResource: "imageNoWidthHeight", ofType: "html")
+    try! imageNoWidthHeight.content = String(contentsOfFile: path!)
+    articles.append(imageNoWidthHeight)
+    
+    let manyIframes = Article()
+    manyIframes.title = "Many iFrames"
+    path = bundle.path(forResource: "manyIframes", ofType: "html")
+    try! manyIframes.content = String(contentsOfFile: path!)
+    articles.append(manyIframes)
+    
+    let iframeNoHeight = Article()
+    iframeNoHeight.title = "iFrame No Height"
+    path = bundle.path(forResource: "iframeNoHeight", ofType: "html")
+    try! iframeNoHeight.content = String(contentsOfFile: path!)
+    articles.append(iframeNoHeight)
+    
+    let iframeNoWidth = Article()
+    iframeNoWidth.title = "iFrame No Width"
+    path = bundle.path(forResource: "iframeNoWidth", ofType: "html")
+    try! iframeNoWidth.content = String(contentsOfFile: path!)
+    articles.append(iframeNoWidth)
+    
+    let iframeNoWidthHeight = Article()
+    iframeNoWidthHeight.title = "iFrame No Width or Height"
+    path = bundle.path(forResource: "iframeNoWidthHeight", ofType: "html")
+    try! iframeNoWidthHeight.content = String(contentsOfFile: path!)
+    articles.append(iframeNoWidthHeight)
+    
+    let linkedImage = Article()
+    linkedImage.title = "Linked Image"
+    path = bundle.path(forResource: "linkedImage", ofType: "html")
+    try! linkedImage.content = String(contentsOfFile: path!)
+    articles.append(linkedImage)
+    
+    let linkEmpty = Article()
+    linkEmpty.title = "Link Empty"
+    path = bundle.path(forResource: "linkEmpty", ofType: "html")
+    try! linkEmpty.content = String(contentsOfFile: path!)
+    articles.append(linkEmpty)
+    
+    let linkNoHref = Article()
+    linkNoHref.title = "Link No Href"
+    path = bundle.path(forResource: "linkNoHref", ofType: "html")
+    try! linkNoHref.content = String(contentsOfFile: path!)
+    articles.append(linkNoHref)
     
     return articles
 }
